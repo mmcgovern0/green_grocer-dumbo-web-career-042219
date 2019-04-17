c9e7517b504548d05858fb732384658c02c7fbca
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 02:33:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
 require 'pry'
-cart = 
+cart =  [
+  {"AVOCADO" => {:price => 3.00, :clearance => true}},
+  {"KALE" => {:price => 3.00, :clearance => false}},
+  {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+  {"ALMONDS" => {:price => 9.00, :clearance => false}},
+  {"TEMPEH" => {:price => 3.00, :clearance => true}},
+  {"CHEESE" => {:price => 6.50, :clearance => false}},
+  {"BEER" => {:price => 13.00, :clearance => false}},
+  {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+  {"BEETS" => {:price => 2.50, :clearance => false}},
+  {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]
 
 
 def consolidate_cart(cart)

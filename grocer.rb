6858afcb76acfecad8608c5306dc9fc8c0179811
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 02:16:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,25 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  organized_cart = {}
+  count = 0
+  cart.each do |element|
+    element.each do |item, hash|
+      organized_cart[item] ||= hash
+      organized_cart[item][:count] ||= 0
+      organized_cart[item][:count] += 1
+    end
+  end
+  organized_cart
 end
 
 def apply_coupons(cart, coupons)
+
+end
+
+def apply_clearance(cart: [])
   # code here
 end
 
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
+def checkout(cart: [], coupons: [])
   # code here
 end

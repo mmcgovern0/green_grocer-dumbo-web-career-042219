f397434b4d9f3e085add7e6f91cd8265bdeb2dfd
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 03:01:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -61,8 +61,8 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  good_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(good_cart(cart), coupons)
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   total = 0
   final_cart.each do |food, info|
@@ -73,8 +73,6 @@ def checkout(cart, coupons)
     total
   end
 end
-
-
 
 
 

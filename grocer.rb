1018c18e8125efd891a3a8a091c453a3837ee8a5
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 02:42:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,27 +48,6 @@ def apply_coupons(cart, coupons)
     cart.merge(couponed_items)
 end
 
-  couponed_items= {}
-  coupons.each do |coupon_hash|
-    cart.each do |name, item_hash|
-      if name == coupon_hash[:item] && item_hash[:count] >= coupon_hash[:num]
-        item_hash[:count] = item_hash[:count] - coupon_hash[:num]
-        new_name = coupon_hash[:item] + " w/coupon"
-        couponed_items[new_name] = { :price => coupon_hash[:cost], 
-                                     :count => 1, 
-                                     :clearance => item_hash[:clearance] }
-      end 
-    end
-  end
-  cart.merge(couponed_items)
-end
-
-
-
-
-
-
-
 
 def apply_clearance(cart: [])
   # code here
